6f8e4072014e30a2f03e4eec4872d3cc5576b77e
cliffgla <cliffordgagliardo@gmail.com>
2017-09-28 18:42:41 +0100

    made toggle function extensible

--- scripts.js
+++ scripts.js
@@ -1,27 +1,7 @@
 //Toggle editing windows
-function toggleBrightness() {
-    var x = document.getElementById('brightness')
-    if (x.style.display=== "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-
-}
-
-function toggleContrast() {
-    var x = document.getElementById('contrast')
-    if (x.style.display=== "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-
-}
-
-function toggleSaturation() {
-    var x = document.getElementById('saturation')
-    if (x.style.display=== "none") {
+function toggle(id) {
+    var x = document.getElementById(id)
+    if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
