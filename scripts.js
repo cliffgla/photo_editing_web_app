2f838c1bc094b32b436b7385e60b518119a9e54f
cliffgla <cliffordgagliardo@gmail.com>
2017-09-28 12:37:47 +0100

    added menu toggling to editor control menu

---
+++ scripts.js
@@ -0,0 +1,30 @@
+//Toggle editing windows
+function toggleBrightness() {
+    var x = document.getElementById('brightness')
+    if (x.style.display=== "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function toggleContrast() {
+    var x = document.getElementById('contrast')
+    if (x.style.display=== "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function toggleSaturation() {
+    var x = document.getElementById('saturation')
+    if (x.style.display=== "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
