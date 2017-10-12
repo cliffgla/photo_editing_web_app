94f80faa010ef7e3c9781d75bae8654445097613
cliffgla <cliffordgagliardo@gmail.com>
2017-10-12 10:58:22 +0100

    added preliminary Javascript script to show value of sliders

--- scripts.js
+++ scripts.js
@@ -9,3 +9,12 @@ function toggle(id) {
     }
 
 }
+
+var slider = document.getElementById('brightSlide');
+var output = document.getElementById('range-value');
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+    output.innerHTML = this.value; 
+
+}
