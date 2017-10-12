ad554875870e27c3f9ac1cbfc3224e593f8bd79a
cliffgla <cliffordgagliardo@gmail.com>
2017-10-12 14:09:11 +0100

    fixed javascript that only targted one value of control title

--- scripts.js
+++ scripts.js
@@ -14,29 +14,29 @@ function toggle(id) {
 */
 
 
-var slider = document.getElementById('brightSlide');
-var output = document.getElementById('brightness-value');
-output.innerHTML = slider.value;
+var slider1 = document.getElementById('brightSlide');
+var output1 = document.getElementById('brightness-value');
+output1.innerHTML = slider1.value;
 
-slider.oninput = function() {
-    output.innerHTML = this.value;
-
-}
-
-var slider = document.getElementById('contrastSlide');
-var output = document.getElementById('contrast-value');
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-    output.innerHTML = this.value;
+slider1.oninput = function() {
+    output1.innerHTML = this.value;
 
 }
 
-var slider = document.getElementById('saturationSlide');
-var output = document.getElementById('saturation-value');
-output.innerHTML = slider.value;
+var slider2 = document.getElementById('contrastSlide');
+var output2 = document.getElementById('contrast-value');
+output2.innerHTML = slider2.value;
 
-slider.oninput = function() {
-    output.innerHTML = this.value;
+slider2.oninput = function() {
+    output2.innerHTML = this.value;
+
+}
+
+var slider3 = document.getElementById('saturationSlide');
+var output3 = document.getElementById('saturation-value');
+output3.innerHTML = slider3.value;
+
+slider3.oninput = function() {
+    output3.innerHTML = this.value;
 
 }
