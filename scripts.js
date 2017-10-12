8529ffdbed30f16c2bb31e5eada414b9a661bff7
cliffgla <cliffordgagliardo@gmail.com>
2017-10-12 11:09:58 +0100

    commented out toggle function in JS script, just in case used later

--- scripts.js
+++ scripts.js
@@ -10,11 +10,12 @@ function toggle(id) {
 
 }
 
+
 var slider = document.getElementById('brightSlide');
 var output = document.getElementById('range-value');
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
-    output.innerHTML = this.value; 
+    output.innerHTML = this.value;
 
 }
