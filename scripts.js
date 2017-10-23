302e73b0054de36d58093306ca639e0a4e55d376
cliffgla <cliffordgagliardo@gmail.com>
2017-10-23 11:50:34 +0100

    replaced capturing slider data with jquery

--- scripts.js
+++ scripts.js
@@ -1,4 +1,41 @@
+var brightVal;
 
+var contrastVal;
+
+var satVal;
+
+$("input[value]").change(function() {
+    //brightness control
+    brightVal = $("#brightness-input").val();
+    pVal = parseInt(brightVal);
+    console.log(brightVal);
+
+    brightVal = (pVal+10)/10.0;
+
+    $("#brightness-value").html("<p>"+ pVal + "</p>");
+    $("#image-load").css("filter", "brightness("+ brightVal +") contrast("+ contrastVal +") saturate(" + satVal + ")");
+
+    //contrast control
+    contrastVal = $("#contrast-input").val();
+    pVal = parseInt(contrastVal);
+    console.log(contrastVal);
+
+    contrastVal = (pVal+10)/10.0;
+
+    $("#contrast-value").html("<p>"+ pVal + "</p>");
+    $("#photo img").css("filter", "brightness("+ brightVal +") contrast("+ contrastVal +") saturate(" + satVal + ")");
+
+    //saturation control
+    satVal = $("#saturation-input").val();
+    pVal = parseInt(satVal);
+    console.log(contrastVal);
+
+    satVal = (pVal+10)/10.0;
+
+    $("#saturation-value").html("<p>"+ pVal + "</p>");
+    $("#photo img").css("filter", "brightness("+ brightVal +") contrast("+ contrastVal +") saturate(" + satVal + ")");
+
+});
 
 var uploadBtn = document.getElementById("photo-load")
 
